205177572a7cbf93c6b2dd48ab3da2ee854acd4c
sonohoshi <yd111544@gmail.com>
2020-12-05 23:43:41 +0900

    Add Girl&Boy skeleton prefabs

---
+++ HANABI_rhythm game/Assets/Scripts/IngameSetCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class IngameSetCharacter : MonoBehaviour
+{
+    public int n { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        var skeleton = GetComponent<SkeletonAnimation>();
+        var skeleton2 = GetComponent<SkeletonAnimation>();
+        if (GameManager.FemaleCharactorSelect)
+        {
+            
+        }
+        else if (GameManager.MaleCharactorSelect)
+        {
+            
+        }
+        else
+        {
+            Debug.Log("fucking bug");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
